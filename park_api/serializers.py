eb992f59e2dd0bca7ef9f03da3e47a57f274f92b
Vasyl Polianskyi <buzzpolyansky@gmail.com>
2021-12-14 23:27:41 +0200

    change serializers

--- park_api/serializers.py
+++ park_api/serializers.py
@@ -3,15 +3,13 @@ from park_api import models
 
 
 class DriverSerializer(serializers.HyperlinkedModelSerializer):
-    cars = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
     class Meta:
         model = models.Driver
         fields = ['id', 'first_name', 'last_name', 'created_at', 'updated_at']
 
 
 class VehicleSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='driver.last_name')
+    driver_id = serializers.ReadOnlyField(source='driver.last_name')
 
     class Meta:
         model = models.Vehicle
